1d4cd2db3955a2854dbc21ff65f7d8cf88709bdb
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2021-08-27 10:43:12 +0200

    Added rounding function

--- calculator_files/script.js
+++ calculator_files/script.js
@@ -6,18 +6,20 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#calculate").addEventListener("click", calculate);
 });
 
+const checkbox = document.querySelector("#doround");
 const results = document.querySelector("#results");
 const number1 = document.querySelector("#firstnumber");
 const number2 = document.querySelector("#secondnumber");
 let operator;
 let result;
+let rounding;
 let resultValue;
 
 function calculate() {
   //Get operator
   operator = document.querySelector("#operator").value;
 
-  //Calculate result
+  //Calculate result based on given operation
   if (operator == "add") {
     resultValue = parseInt(number1.value) + parseInt(number2.value);
   } else if (operator == "sub") {
@@ -26,6 +28,14 @@ function calculate() {
     resultValue = parseInt(number1.value) * parseInt(number2.value);
   } else if (operator == "div") {
     resultValue = parseInt(number1.value) / parseInt(number2.value);
+  }
+
+  if (checkbox.checked === true) {
+    //Get amound of decimals to round to
+    rounding = parseInt(document.querySelector("#decimals").value);
+
+    //Change the result based on number of decimals rounded to
+    resultValue = resultValue.toFixed(rounding);
   }
 
   //Post result as value of first input
