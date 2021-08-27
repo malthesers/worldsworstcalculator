ffaad872e98dc97bb06aa9adbd574ec3617fff0e
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2021-08-27 11:12:10 +0200

    Basics.js changed

--- basics.js
+++ basics.js
@@ -4,4 +4,28 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOMContentLoaded");
 });
 
-let y = 14;
+const bool = true;
+const num = 41;
+const str = "Peter";
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+const nothing = null;
+let undf;
+const symbol = Symbol("symbol");
+const array = [1, 2, 3];
+
+const n1 = 1;
+const n2 = 2;
+
+const s1 = "1";
+const s2 = "2";
+
+const value = ;
+
+if (value) {
+  console.log("Value is truthy");
+} else {
+  console.log("Value is falsy");
+}
