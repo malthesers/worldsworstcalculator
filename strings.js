d671578a7818f9c07f18cdf3ce9356139167c9f8
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2021-08-30 08:50:29 +0200

    Template literals done

---
+++ strings.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const name = "Peter";
+const drink = "Pepsi MAX";
+
+console.log(`Yo ${name}, would you like some ${drink}?`);
---
+++ template.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let name = `Malthe`;
+let animalName = `Monium`;
+
+console.log(`My name is ${name}.\nI have a panda called ${animalName}.`)
