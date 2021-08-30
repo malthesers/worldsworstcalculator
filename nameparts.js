3a500f54bc036971a3d32370e1ee071ea25dce37
Malthe Kusk Lauritsen <malt9830@stud.kea.dk>
2021-08-30 14:29:56 +0200

    Name parts exercise

---
+++ nameparts.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const fullNameAlso = "Malthe Kusk Lauritsen";
+const fullName = "Malthe Vincent Worning Kusk Lauritsen";
+
+const firstName = fullName.substring(0, fullName.indexOf(" "));
+const middleName = fullName.substring(
+  fullName.indexOf(" ") + 1,
+  fullName.lastIndexOf(" ")
+);
+const lastName = fullName.substring(fullName.lastIndexOf(" ") + 1);
+console.log(`First name: ${firstName}`);
+console.log(`Middle name(s): ${middleName}`);
+console.log(`Last name: ${lastName}`);
